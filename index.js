ed0aff9b8399dc661dd03af38ab7b0882ba7bea2
OnyJ <julien.josselin75@hotmail.fr>
2020-04-08 01:23:38 +0200

    functionnal indexes

--- index.js
+++ index.js
@@ -1,53 +1,96 @@
-const checkLoto = (firstname, lastname, email, lotoNumbers) => {
-  checkFillingWhenSubmit(firstname, lastname, email, lotoNumbers)
-  checkEmailWhenSubmit(email)
+let form  = document.getElementsByTagName('form')[0]
+let winningNumbers = [1,2,3,4,5,6]
+let errorMessages = []
+const message = document.getElementById("message")
+
+
+let validateEmail = resource => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(resource).toLowerCase())
 }
 
-let checkFillingWhenSubmit = (firstname, lastname, email, lotoNumbers) => {
-  let form            = document.getElementsByTagName('form')[0]
-  let firstnameError  = document.getElementById('firstnameError')
-  let lastnameError   = document.getElementById('lastnameError')
-  let emailError      = document.getElementById('emailError')
+let validate = () => {
+  let firstName = document.getElementById('first-name').value
+  let lastName = document.getElementById('last-name').value
+  let email = document.getElementById('email').value
+  if(firstName == "") {
+    errorMessages.push("Veuillez fournir un prénom")
+    return false
+    }
+  if(lastName == "") {
+    errorMessages.push("Veuillez fournir un nom")
+    return false
+  }
+  if(email == "") {
+    errorMessages.push("Veuillez fournir un email")
+    return false
+  }
+  if(!validateEmail(email)) {
+    errorMessages.push("Votre email n'est pas valide")
+    return false
+  }
+  return true
+}
 
-  form.addEventListener("submit", function (event) {
-    if (firstname === "") { firstnameError.innerHTML = "Veuillez fournir un prénom" } 
-    if (lastname === "")  { lastnameError.innerHTML = "Veuillez fournir un nom" } 
-    if (email === "")     { emailError.innerHTML = "Veuillez fournir un email" } 
+let getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+  
+let getArrayOfSixRandomNumbers = () => {
+  winningNumbers = [];
+  for (var i = 0; i < 6; i++){
+    winningNumbers.push(getRandomIntInclusive(1, 49));
+  }
+}
+
+let checkVictory = winningNums => {
+  let num1 = document.getElementById('number1').value
+  let num2 = document.getElementById('number2').value
+  let num3 = document.getElementById('number3').value
+  let num4 = document.getElementById('number4').value
+  let num5 = document.getElementById('number5').value
+  let num6 = document.getElementById('number6').value
+  let numbers = [Number(num1), Number(num2), Number(num3), Number(num4), Number(num5), Number(num6)].sort()
+  console.log("Input : " + numbers)
+  for (var i = 0 ; i < 6 ; i++){
+    if (numbers[i] == 0){
+      errorMessages.push("Vous devez choisir 6 numéros")
+      return false
+    } else {
+      for (var j = 0 ; j < numbers.length ; j++) {
+        if (numbers[j] != winningNums[j]){
+          message.innerHTML = "Désolé vous avez perdu"
+          return false
+        }
+      }
+      message.innerHTML = "Félicitations vous avez gagné 1 million !!!!!"
+      return true
+    }
+  }
+}
+
+let errors = errorMsgs => {
+  errorMsgs.forEach(errorMessage => {
+    message.innerHTML = errorMessage
   })
 }
 
-let checkEmailWhenSubmit = (email) => {
-  // let email_rule = 
+let checkLoto = () => {
+  window.addEventListener("DOMContentLoaded", () => {
+    form.addEventListener('submit', event => {
+      event.preventDefault()
+      message.innerHTML = ""
+      getArrayOfSixRandomNumbers()
+      console.log("Jackpot is : " + winningNumbers)    
+      if (validate() == true) {
+        message.innerHTML = ""
+        checkVictory(winningNumbers)  
+      }
+      errors(errorMessages)
+    })
+  })
 }
 
-checkLoto(
-  document.getElementById("firstname").value,
-  document.getElementById("lastname").value,
-  document.getElementById("email").value,
-  document.getElementById("lotoNumbers").value
-)
-
-
-// ([A-Za-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3})$
-
-
-// let form  = document.getElementsByTagName('form')[0];
-// let email = document.getElementById('email');
-// let error = document.querySelector('.error');
-
-// Each time we have input, check validity
-// email.addEventListener("input", function (event) {
-//   if (email.validity.valid) {
-//     error.innerHTML = ""
-//     error.className = "error"
-//   }
-// }, false);
-
-// Each submit we check email validity
-// form.addEventListener("submit", function (event) {
-//   if (!email.validity.valid) {
-//     error.innerHTML = "J'attends une adresse e-mail correcte, mon cher !";
-//     error.className = "error active";
-//     event.preventDefault();
-//   }
-// }, false);
+checkLoto()
